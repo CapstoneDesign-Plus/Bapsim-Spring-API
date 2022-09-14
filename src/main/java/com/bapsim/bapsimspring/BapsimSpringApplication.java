835c10a29ff2e8ecf3cbb98f7d6c2e4ad79000d0
byungjin <byungjin.dev@gmail.com>
2022-09-14 21:40:29 +0900

    init2

---
+++ src/main/java/com/bapsim/bapsimspring/BapsimSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bapsim.bapsimspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapsimSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapsimSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bapsim/bapsimspring/BapsimSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bapsim.bapsimspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapsimSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
